98aafc102e01ca60ce3ed46201b2e48e2b089cef
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2020-08-07 20:28:52 -0300

    Added eslint

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
 app.listen(3333);
